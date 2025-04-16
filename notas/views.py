456b9359ad3d4cc1429eb2319a909a13e19562d1
Andres Martinez <andresfmartinez2706@gmail.com>
2025-04-16 18:58:57 -0500

    cambios varios al codigo

--- notas/views.py
+++ notas/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect,get_object_or_404
-from .forms import NotaForm, UserCreationFormWithImage, ProfileImageForm
+from .forms import NotaForm, UserCreationFormWithImage
 from .models import Nota, Categoria
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
